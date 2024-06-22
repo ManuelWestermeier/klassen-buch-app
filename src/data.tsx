a804cf6b957da265ecd3e4464723556e11cea906
ManuelWestermeier <westermeier111@gmail.com>
2024-06-22 22:25:20 +0200

    update

--- src/data.tsx
+++ src/data.tsx
@@ -1,2 +1,2 @@
 export const apiUrl =
-    document.location.protocol == "http:" ? "http://localhost:8080" : ""
+    document.location.protocol == "http:" ? "http://localhost:8080" : "https://7vqlj6ck-8080.euw.devtunnels.ms/"
